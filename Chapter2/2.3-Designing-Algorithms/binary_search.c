8e10a8ee5a5dae5063d7273bb5189d8cf683077d
Ziyang Jin <f4a0b@ugrad.cs.ubc.ca>
2016-05-30 01:51:09 -0700

    Add files via upload

--- Chapter2/2.3-Designing-Algorithms/binary_search.c
+++ Chapter2/2.3-Designing-Algorithms/binary_search.c
@@ -2,22 +2,23 @@
 #define N 100
 // assume a[l ... r] is sorted
 int binary_search(int* a, int l, int r, int x){
-    if(l <= r) {
+    if(x <= a[l]) return l;
+    else if(x > a[r]) return r+1;
+    else if(l < r) {
         int m = (l + r) / 2;
-        if(x == a[m])
+        if(x <= a[m] && x > a[m-1])
             return m;
-        else if(x < a[m])
+        else if(x <= a[m-1])
             return binary_search(a, l, m-1, x);
         else
             return binary_search(a, m+1, r, x);
-    } else
-        return -1;
+    }
 }
 
 int main(){
     int a[N];
     for(int i = 0; i < N; i++)
-        a[i] = i;
+        a[i] = 3*i;
     printf("%d\n", binary_search(a, 0, N-1, 233));
     printf("%d\n", binary_search(a, 0, N-1, 10));
     printf("%d\n", binary_search(a, 0, N-1, 73));
