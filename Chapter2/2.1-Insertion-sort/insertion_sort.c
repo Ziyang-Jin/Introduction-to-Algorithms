434c1cd33216dcb700f48584117ce5b451349793
Ziyang Jin <f4a0b@ugrad.cs.ubc.ca>
2016-05-29 19:33:41 -0700

    Update and rename insertion_sort.cpp to insertion_sort.c

---
+++ Chapter2/2.1-Insertion-sort/insertion_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define N 10
+
+void insertion_sort(int* a, int n) {
+    int i, key;
+    for(int j = 1; j < n; j++) {
+        key = a[j];
+        i = j - 1;
+        while(i >= 0 && a[i] > key) {
+            a[i+1] = a[i];
+            i--;
+        }
+        a[i+1] = key;
+    }
+}
+
+void print_array(int* a, int n) {
+    printf("Array: [");
+    for(int i = 0; i < n-1; i++)
+        printf("%d, ", a[i]);
+    printf("%d]\n", a[n-1]);
+}
+
+int main() {
+    int a[N] = {12, 23, 1, 4, 67, 33, 82, 5, 4, 15};
+    print_array(a, N);
+    insertion_sort(a, N);
+    print_array(a, N);
+}
