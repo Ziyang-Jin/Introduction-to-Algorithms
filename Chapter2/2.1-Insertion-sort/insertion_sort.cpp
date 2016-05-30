49dc7e36c90fa9d12d06ed264e393b3c6165833a
Ziyang Jin <f4a0b@ugrad.cs.ubc.ca>
2016-05-29 19:22:34 -0700

    Update insertion_sort.cpp

--- Chapter2/2.1-Insertion-sort/insertion_sort.cpp
+++ Chapter2/2.1-Insertion-sort/insertion_sort.cpp
@@ -1,24 +1,12 @@
-#include <iostream>
-
-int* insertion_sort(int* A, int n) {
+void insertion_sort(int* a, int n) {
+    int i, key;
     for(int j = 1; j < n; j++) {
-        int key = A[j];
-        // insert A[j] into the sorted sequence A[1 ... j-1]
-        int i = j - 1;
-        while(i && A[i] > key) {
-            A[i+1] = A[i];
+        key = a[j];
+        i = j - 1;
+        while(i >= 0 && a[i] > key) {
+            a[i+1] = a[i];
             i--;
         }
-        A[i+1] = key;
+        a[i+1] = key;
     }
-return A;
-}
-
-int main() {
-    int a[10] = {12, 23, 1, 4, 67, 33, 82, 1, 7, 15};
-    int* b = insertion_sort(a, 10);
-    for(int i = 0; i < 10; i++)
-        std::cout << b[i] << " ";
-    std::cout << std::endl;
-return 0;
 }
